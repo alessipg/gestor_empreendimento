fb9269e802da0661dbca7d707a83dfb7fd9248de
Gabriel Alessi <gabriel.alessip@gmail.com>
2025-12-02 14:15:16 -0300

    fix(service): now each API has your own service

--- lib/services/viacep_service.dart
+++ lib/services/viacep_service.dart
@@ -25,32 +25,4 @@ class ViaCepService {
     }
     return null;
   }
-
-  static Future<Map<String, dynamic>?> buscarCnpj(String cnpj) async {
-    final cnpjLimpo = cnpj.replaceAll(RegExp(r'[^0-9]'), '');
-    if (cnpjLimpo.length != 14) return null;
-
-    final url = Uri.parse('https://www.receitaws.com.br/v1/cnpj/$cnpjLimpo');
-    try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        if (data['status'] == 'ERROR') return null;
-        return {
-          'nome': data['nome'] ?? data['fantasia'],
-          'telefone': data['telefone']?.replaceAll(RegExp(r'[^0-9]'), ''),
-          'email': data['email'],
-          'cep': data['cep']?.replaceAll(RegExp(r'[^0-9]'), ''),
-          'logradouro': data['logradouro'],
-          'numero': data['numero'],
-          'bairro': data['bairro'],
-          'municipio': data['municipio'],
-          'uf': data['uf'],
-        };
-      }
-    } catch (e) {
-      print('Erro ao buscar CNPJ: $e');
-    }
-    return null;
-  }
 }
--- lib/views/pages/fornecedores/fornecedor_criar.dart
+++ lib/views/pages/fornecedores/fornecedor_criar.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:receitacerta/config/constants.dart';
 import 'package:receitacerta/controllers/fornecedor_controller.dart';
 import 'package:receitacerta/services/viacep_service.dart';
+import 'package:receitacerta/services/receitaws_service.dart';
 
 class FornecedorCriar extends StatefulWidget {
   const FornecedorCriar({super.key});
@@ -92,7 +93,7 @@ class _FornecedorCriarState extends State<FornecedorCriar> {
       _isLoadingCnpj = true;
     });
 
-    final resultado = await ViaCepService.buscarCnpj(cnpj);
+    final resultado = await ReceitawsService.buscarCnpj(cnpj);
 
     setState(() {
       _isLoadingCnpj = false;
